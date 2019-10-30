3d8431f0e6bfb35fcbcb30b5278bd11e329bf6bf
Jessica Lovell <jessicaamberlovell@gmail.com>
2019-10-30 15:08:36 -0700

    Initial Commit

---
+++ src/main/java/com/powderroom/PowderroomApplication.java
@@ -0,0 +1,13 @@
+package com.powderroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowderroomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowderroomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/powderroom/PowderroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powderroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowderroomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
